44e2dc99781d4aaf16368678fb26034d006473ec
sunil <sunildutt6@hotmail.com>
2022-10-15 14:13:51 +0200

    commit

--- src/main/java/com/sunil/userdatajpaproject/controller/UserController.java
+++ src/main/java/com/sunil/userdatajpaproject/controller/UserController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.sunil.userdatajpaproject.entities.User;
 import com.sunil.userdatajpaproject.exception.handlerException;
